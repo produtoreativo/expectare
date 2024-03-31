9c641b33e3170c9f7c8ece77a493dc7654ffadc8
Christiano Milfont <cmilfont@gmail.com>
2024-03-31 00:13:58 -0300

    Deploy first strategy

--- android/app/src/main/java/com/produtoreativo/expectare/MainActivity.kt
+++ android/app/src/main/java/com/produtoreativo/expectare/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.expectare
+package com.produtoreativo.expectare
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/produtoreativo/expectare/MainApplication.kt
+++ android/app/src/main/java/com/produtoreativo/expectare/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.expectare
+package com.produtoreativo.expectare
 
 import android.app.Application
 import com.facebook.react.PackageList
