7f506943c925998ac5040f3c09bb9da2c37f0744
Christiano Milfont <cmilfont@gmail.com>
2024-04-09 16:28:17 -0300

    Add icons to Android

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '@expectare/screens/Home';
 import Settings from '@expectare/screens/Settings';
 import Workspace from '@expectare/screens/Workspace';
+import Icon from 'react-native-vector-icons/FontAwesome6';
 
 const Tab = createBottomTabNavigator();
 
@@ -11,18 +12,37 @@ function App(): React.JSX.Element {
  
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: '#FFC107'
+        }}
+      >
         <Tab.Screen 
           name="Home" 
           component={HomeScreen} 
+          options={{
+            tabBarIcon: ({ color }) => ( // Use tabBarIcon prop for icon
+              <Icon name="house" size={24} color={color} />
+            ),
+          }}
         />
         <Tab.Screen 
           name="Workspace" 
           component={Workspace} 
+          options={{
+            tabBarIcon: ({ color }) => ( // Use tabBarIcon prop for icon
+              <Icon name="briefcase" size={24} color={color} />
+            ),
+          }}
         />
         <Tab.Screen 
           name="Settings" 
           component={Settings} 
+          options={{
+            tabBarIcon: ({ color }) => ( // Use tabBarIcon prop for icon
+              <Icon name="gear" size={24} color={color} />
+            ),
+          }}
         />
       </Tab.Navigator>
     </NavigationContainer>
