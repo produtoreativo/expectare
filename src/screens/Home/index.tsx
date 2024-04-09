27bd07aa9007cf9be802631429cedfa74040122f
Christiano Milfont <cmilfont@gmail.com>
2024-04-09 00:58:20 -0300

    Install dependency: react-navigation

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+function Home() {
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home</Text>
+    </View>
+
+  )
+}
+
+export default Home;
---
+++ src/screens/Settings/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+function Settings() {
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Settings</Text>
+    </View>
+
+  )
+}
+
+export default Settings;
