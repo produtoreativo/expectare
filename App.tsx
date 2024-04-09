122e80e735b5d86b3f7d4162f65db44099691520
Christiano Milfont <cmilfont@gmail.com>
2024-04-09 01:08:28 -0300

    Create TAB option for Workspace module

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './src/screens/Home';
 import Settings from './src/screens/Settings';
+import Workspace from './src/screens/Workspace';
 
 const Tab = createBottomTabNavigator();
 
@@ -14,6 +15,10 @@ function App(): React.JSX.Element {
         <Tab.Screen 
           name="Home" 
           component={HomeScreen} 
+        />
+        <Tab.Screen 
+          name="Workspace" 
+          component={Workspace} 
         />
         <Tab.Screen 
           name="Settings" 
---
+++ src/screens/Workspace/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+function Workspace() {
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Workspace</Text>
+    </View>
+
+  )
+}
+
+export default Workspace;
